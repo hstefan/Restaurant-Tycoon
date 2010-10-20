e7fd74e580ed9fe0a47b3a2e6da08a027913a724
Hugo Stefan Kaus Puhlmann <hugopuhlmann@gmail.com>
2010-10-20 13:07:06 -0200

    main file used for tests

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "src\htl\hvector.h"
+#include <iostream>
+
+int main()
+{
+	htl::vector<int> a;
+	a.push_back(1);
+	a.push_back(2);
+	a.push_back(3);
+	for(htl::vector<int>::iterator it = a.begin(); it != a.end(); it++)
+	{
+		std::cout << *it;
+	}
+	getchar();
+	getchar();
+}
