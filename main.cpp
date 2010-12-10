ebe5d00ea528f7d28191e17f167d1a7a64f16a0b
Hugo Puhlmann <hugopuhlmann@gmail.com>
2010-12-10 17:51:25 -0200

    main.cpp

--- main.cpp
+++ main.cpp
@@ -5,30 +5,6 @@
 
 int main()
 {
-	/*rty::SortedList<int> a;
-	a.insert(5);
-	a.insert(3);
-	a.insert(4);
-	a.insert(7);
-	a.insert(1);
-	a.insert(9);
-
-	for(rty::SortedList<int>::iterator it = a.begin(); it != a.end(); it++)
-	{
-		std::cout << *it << std::endl;
-	}
-
-	rty::SearchBinTree<int, double> i;
-	i.insert(1, 2.3);
-	i.insert(2, 4.3);
-	i.search(1);
-	i.remove(1);
-	i.remove(2);
-	i.remove(3);*/
-
 	rty::UserInterface ui;
 	ui.start();
-
-	getchar();
-	getchar();
 }
